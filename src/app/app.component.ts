35b72b55e224827095e00ec9056ab05e16df97a8
Rufai-Ahmed <abbeyrufai234@gmail.com>
2024-08-04 22:09:29 +0100

    all task implemented

---
+++ src/app/app.component.ts
@@ -0,0 +1,60 @@
+import { Component, AfterViewInit, OnInit, NgZone } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { MobileSiderComponent } from './mobile-sider/mobile-sider.component';
+import { timer } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    SidebarComponent,
+    CommonModule,
+    HeaderComponent,
+    NgOptimizedImage,
+    FooterComponent,
+    ContentComponent,
+    MobileSiderComponent,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent implements OnInit {
+  title = 'Univacity';
+
+  selectedFilters: string[] = [];
+
+  onFiltersChanged(filters: string[]) {
+    this.selectedFilters = filters;
+  }
+
+  timer: any;
+
+  isDarkMode: boolean = false;
+
+  toggleDarkMode: (theme: boolean) => void = (theme: boolean) =>
+    (this.isDarkMode = theme);
+
+  isShow: boolean = false;
+
+  constructor(private ngZone: NgZone) {}
+
+  ngOnInit() {
+    this.ngZone.runOutsideAngular(() => {
+      this.timer = setInterval(() => {
+        this.ngZone.run(() => {
+          this.isShow = true;
+        });
+      }, 6000);
+    });
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timer);
+  }
+}
