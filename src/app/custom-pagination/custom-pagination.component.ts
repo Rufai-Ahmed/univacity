6713c1a631cf19d4b877326f72aaf0ce8e1bec1a
Rufai-Ahmed <abbeyrufai234@gmail.com>
2024-08-05 04:51:40 +0100

    fixed pagination responsiveness issues

--- src/app/custom-pagination/custom-pagination.component.ts
+++ src/app/custom-pagination/custom-pagination.component.ts
@@ -1,44 +1,18 @@
-import { CommonModule, isPlatformBrowser } from '@angular/common';
-import {
-  Component,
-  Input,
-  Output,
-  EventEmitter,
-  OnInit,
-  Renderer2,
-  ElementRef,
-  Inject,
-  PLATFORM_ID,
-} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-custom-pagination',
-  templateUrl: './custom-pagination.component.html',
   standalone: true,
-  styleUrls: ['./custom-pagination.component.scss'],
   imports: [CommonModule],
+  templateUrl: './custom-pagination.component.html',
+  styleUrls: ['./custom-pagination.component.scss'],
 })
-export class CustomPaginationComponent implements OnInit {
-  isDarkMode = false;
-
+export class CustomPaginationComponent {
   @Input() totalItems: number = 0;
   @Input() pageSize: number = 10;
   @Input() currentPage: number = 1;
   @Output() pageChange = new EventEmitter<number>();
-
-  constructor(
-    private renderer: Renderer2,
-    private el: ElementRef,
-    @Inject(PLATFORM_ID) private platformId: Object
-  ) {}
-
-  ngOnInit() {
-    if (isPlatformBrowser(this.platformId)) {
-      this.isDarkMode = window.matchMedia(
-        '(prefers-color-scheme: dark)'
-      ).matches;
-    }
-  }
 
   get totalPages(): number {
     return Math.ceil(this.totalItems / this.pageSize);
@@ -46,40 +20,45 @@ export class CustomPaginationComponent implements OnInit {
 
   get pages(): (number | string)[] {
     const totalPages = this.totalPages;
-    const pages: (number | string)[] = [];
-    const delta = 5;
+    const currentPage = this.currentPage;
 
-    if (totalPages <= 5) {
-      for (let i = 1; i <= totalPages; i++) {
+    if (totalPages <= 7) {
+      return Array.from({ length: totalPages }, (_, i) => i + 1);
+    }
+
+    const pages: (number | string)[] = [];
+
+    if (currentPage <= 4) {
+      for (let i = 1; i <= 5; i++) {
+        pages.push(i);
+      }
+      pages.push('...');
+      pages.push(totalPages);
+    } else if (currentPage >= totalPages - 3) {
+      pages.push(1);
+      pages.push('...');
+      for (let i = totalPages - 4; i <= totalPages; i++) {
         pages.push(i);
       }
     } else {
       pages.push(1);
-
-      if (this.currentPage > delta + 2) {
-        pages.push(-1);
-      }
-
-      for (
-        let i = Math.max(2, this.currentPage - delta);
-        i <= Math.min(totalPages - 1, this.currentPage + delta);
-        i++
-      ) {
+      pages.push('...');
+      for (let i = currentPage - 1; i <= currentPage + 1; i++) {
         pages.push(i);
       }
-
-      if (this.currentPage < totalPages - delta - 1) {
-        pages.push(-1);
-      }
-
+      pages.push('...');
       pages.push(totalPages);
     }
 
     return pages;
   }
 
+  isNumber(page: number | string): page is number {
+    return typeof page === 'number';
+  }
+
   onPageChange(page: number) {
-    if (page > 0 && page <= this.totalPages) {
+    if (page >= 1 && page <= this.totalPages) {
       this.pageChange.emit(page);
     }
   }
